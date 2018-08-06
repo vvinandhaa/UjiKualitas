f01f1ca6eb2e5dbb72250e6038942600e1e88ff4
ASUS <vvinandha@gmail.com>
2018-08-06 08:44:59 +0700

    cmt3

---
+++ app/src/main/java/com/example/asus/ujikualitas/ExpandableListDataPump.java
@@ -0,0 +1,144 @@
+package com.example.asus.ujikualitas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListDataPump {
+    public static HashMap<String, List<String>> getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+
+        List<String> id1 = new ArrayList<String>();
+        id1.add("PID");
+        id1.add("Materi");
+        id1.add("Nilai");
+        id1.add("Keterangan");
+        id1.add("Batas Atas");
+        id1.add("Batas Bawah");
+        id1.add("Satuan");
+
+        List<String> id2 = new ArrayList<String>();
+        id2.add("PID");
+        id2.add("Materi");
+        id2.add("Nilai");
+        id2.add("Keterangan");
+        id2.add("Batas Atas");
+        id2.add("Batas Bawah");
+        id2.add("Satuan");
+
+        List<String> id3 = new ArrayList<String>();
+        id3.add("PID");
+        id3.add("Materi");
+        id3.add("Nilai");
+        id3.add("Keterangan");
+        id3.add("Batas Atas");
+        id3.add("Batas Bawah");
+        id3.add("Satuan");
+
+        List<String> id4 = new ArrayList<String>();
+        id4.add("PID");
+        id4.add("Materi");
+        id4.add("Nilai");
+        id4.add("Keterangan");
+        id4.add("Batas Atas");
+        id4.add("Batas Bawah");
+        id4.add("Satuan");
+
+        List<String> id5 = new ArrayList<String>();
+        id5.add("PID");
+        id5.add("Materi");
+        id5.add("Nilai");
+        id5.add("Keterangan");
+        id5.add("Batas Atas");
+        id5.add("Batas Bawah");
+        id5.add("Satuan");
+
+        List<String> id6 = new ArrayList<String>();
+        id6.add("PID");
+        id6.add("Materi");
+        id6.add("Nilai");
+        id6.add("Keterangan");
+        id6.add("Batas Atas");
+        id6.add("Batas Bawah");
+        id6.add("Satuan");
+
+        List<String> id7 = new ArrayList<String>();
+        id7.add("PID");
+        id7.add("Materi");
+        id7.add("Nilai");
+        id7.add("Keterangan");
+        id7.add("Batas Atas");
+        id7.add("Batas Bawah");
+        id7.add("Satuan");
+
+        List<String> id8 = new ArrayList<String>();
+        id8.add("PID");
+        id8.add("Materi");
+        id8.add("Nilai");
+        id8.add("Keterangan");
+        id8.add("Batas Atas");
+        id8.add("Batas Bawah");
+        id8.add("Satuan");
+
+        List<String> id9 = new ArrayList<String>();
+        id9.add("PID");
+        id9.add("Materi");
+        id9.add("Nilai");
+        id9.add("Keterangan");
+        id9.add("Batas Atas");
+        id9.add("Batas Bawah");
+        id9.add("Satuan");
+
+        List<String> id10 = new ArrayList<String>();
+        id10.add("PID");
+        id10.add("Materi");
+        id10.add("Nilai");
+        id10.add("Keterangan");
+        id10.add("Batas Atas");
+        id10.add("Batas Bawah");
+        id10.add("Satuan");
+
+        List<String> id11 = new ArrayList<String>();
+        id11.add("PID");
+        id11.add("Materi");
+        id11.add("Nilai");
+        id11.add("Keterangan");
+        id11.add("Batas Atas");
+        id11.add("Batas Bawah");
+        id11.add("Satuan");
+
+        List<String> id12 = new ArrayList<String>();
+        id12.add("PID");
+        id12.add("Materi");
+        id12.add("Nilai");
+        id12.add("Keterangan");
+        id12.add("Batas Atas");
+        id12.add("Batas Bawah");
+        id12.add("Satuan");
+
+        List<String> id13 = new ArrayList<String>();
+        id13.add("PID");
+        id13.add("Materi");
+        id13.add("Nilai");
+        id13.add("Keterangan");
+        id13.add("Batas Atas");
+        id13.add("Batas Bawah");
+        id13.add("Satuan");
+
+        expandableListDetail.put("PH", id1);
+        expandableListDetail.put("TURBIDITY", id2);
+        expandableListDetail.put("DO", id3);
+        expandableListDetail.put("BOD", id4);
+        expandableListDetail.put("COD", id5);
+        expandableListDetail.put("TSS", id6);
+        expandableListDetail.put("NO3N", id7);
+        expandableListDetail.put("NO2N", id8);
+        expandableListDetail.put("PO4P", id9);
+        expandableListDetail.put("DETERGEN", id10);
+        expandableListDetail.put("MINYAK LEMAK", id11);
+        expandableListDetail.put("TOTAL COLI", id12);
+        expandableListDetail.put("TEMP", id13);
+
+        return expandableListDetail;
+    }
+}
